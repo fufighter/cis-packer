29d7269d9d67b94117e710c310a34e0f66a8cc6b
afu <afu>
2024-11-26 10:43:46 -0500

    fix

--- scripts/inspector.sh
+++ scripts/inspector.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
-aws inspector2 create-cis-scan-configuration --schedule "oneTime={}" --scan-name packer --security-level LEVEL_1 --targets "accountIds=$AWS_ACCOUNT_ID,targetResourceTags={packer=$AMI_NAME-$CODEBUILD_BUILD_NUMBER}"
+ACCOUNTID=$(aws sts get-caller-identity --query Account --output text)
+echo $ACCOUNTID
+echo $AWS_ACCOUNT_ID
+
+aws inspector2 create-cis-scan-configuration --schedule "oneTime={}" --scan-name packer --security-level LEVEL_1 --targets "accountIds=$ACCOUNTID,targetResourceTags={packer=$AMI_NAME-$CODEBUILD_BUILD_NUMBER}"
 
 SCANARN=$(aws inspector2 list-cis-scans | jq -r ".scans[] | select(.targets.targetResourceTags.packer==[\"$AMI_NAME-$CODEBUILD_BUILD_NUMBER\"]).scanArn")
 echo $SCANARN
