cb5f6d34cf20ff32eb021eb05e1f1099bb3e4421
afu <afu>
2024-11-26 11:03:29 -0500

    fix

--- scripts/inspector.sh
+++ scripts/inspector.sh
@@ -1,21 +1,20 @@
 #!/bin/sh
 ACCOUNTID=$(aws sts get-caller-identity --query Account --output text)
 echo $ACCOUNTID
-echo $AWS_ACCOUNT_ID
 
 aws inspector2 create-cis-scan-configuration --schedule "oneTime={}" --scan-name packer --security-level LEVEL_1 --targets "accountIds=$ACCOUNTID,targetResourceTags={packer=$AMI_NAME-$CODEBUILD_BUILD_NUMBER}"
 
-aws inspector2 list-cis-scans
+aws inspector2 list-cis-scans | jq -r ".scans[] | select(.targets.targetResourceTags.packer==[\"${AMI_NAME}-${CODEBUILD_BUILD_NUMBER}\"]).scanArn"
 
-SCANARN=$(aws inspector2 list-cis-scans | jq -r ".scans[] | select(.targets.targetResourceTags.packer==[\"$AMI_NAME-$CODEBUILD_BUILD_NUMBER\"]).scanArn")
-echo scanarn $SCANARN
-STATUS=$(aws inspector2 get-cis-scan-report --scan-arn $SCANARN | jq -r '.status')
+SCAN_ARN=$(aws inspector2 list-cis-scans | jq -r ".scans[] | select(.targets.targetResourceTags.packer==[\"${AMI_NAME}-${CODEBUILD_BUILD_NUMBER}\"]).scanArn")
+echo scanarn $SCAN_ARN
+STATUS=$(aws inspector2 get-cis-scan-report --scan-arn $SCAN_ARN | jq -r '.status')
 echo "$STATUS"
 
 while [ "$STATUS" != "SUCCEEDED" ]
 do
   sleep 30
-  STATUS=$(aws inspector2 get-cis-scan-report --scan-arn $SCANARN | jq -r '.status')
+  STATUS=$(aws inspector2 get-cis-scan-report --scan-arn $SCAN_ARN | jq -r '.status')
   echo "$STATUS (wait 30)... "
 done
 
